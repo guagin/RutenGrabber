87e519b2801f4f5f0a122898111807375bb8b74e
張睿馳 <>
2016-05-24 00:41:48 +0800

    WTF

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "index" = "index#index"
+  get "index" => "index#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
